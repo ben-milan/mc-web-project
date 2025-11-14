359373f8397bacc834b54b4927e734de7e4a8549
ben-milan <bmk.8047@gmail.com>
2025-11-14 15:04:07 +0100

    work in progress added

--- frontend/src/components/Whitelist.tsx
+++ frontend/src/components/Whitelist.tsx
@@ -5,7 +5,7 @@ function Whitelist() {
 
   const handleKeyDown = (e) => {
     if (e.key === "Enter") {
-      alert(`"ENTER PRESSED! VALUE: ${value}`);
+      console.log(`"ENTER PRESSED! VALUE: ${value}`);
       // here you could also call a prop callback or send value to backend
     }
   };
@@ -16,7 +16,7 @@ function Whitelist() {
       name="whitelist"
       id="whitelist"
       className="whitelist"
-      placeholder="Enter Player Tag"
+      placeholder="Work in Progress"
       value={value}
       onChange={(e) => setValue(e.target.value)}
       onKeyDown={handleKeyDown}
